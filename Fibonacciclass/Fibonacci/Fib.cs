e033721e2823663709ae125a8557b8adf3f26d63
Araqelyan Tiran <tirans3777@gmail.com>
2019-04-05 21:48:48 +0400

    Add indexator

--- Fibonacciclass/Fibonacci/Fib.cs
+++ Fibonacciclass/Fibonacci/Fib.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
 
 namespace Fibonacci
 {
     class Fib
     {
+        
+        public  BigInteger this[BigInteger i]
+        {
+            get
+            {
+                if (i < 0) throw new ArgumentOutOfRangeException();
+                if (i == 0) return 0;
+                if (i == 1) return 1;
+               BigInteger a = 0;
+               BigInteger b = 1;
+               BigInteger c = 0;
+                for (int j = 2;j<=i;j++)
+                {
+                    c = a + b;
+                    a = b;
+                    b = c;
+                }
+                return c;
+            }
+
+           
+        }
+       
     }
 }
